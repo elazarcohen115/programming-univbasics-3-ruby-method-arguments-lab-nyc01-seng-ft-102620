090092f303aaf2f5242c2400be3f040045cd9bff
elazar cohen <escohen115@gmail.com>
2020-10-09 17:45:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,13 @@
 
 
 
+def introduction (name)
+  "Hi, my name is #{name}."
+end
 
+def introduction_with_language (name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
